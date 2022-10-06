b5eb5d4fb62f1a98e9fe9fde24ea9f3714642965
Francisco <francisco@skylan.com.br>
2022-10-06 14:17:37 -0300

    Finalização do esboço bg+

--- lines/script.js
+++ lines/script.js
@@ -2,14 +2,17 @@ const ulY = document.querySelector("ul.lines_y");
 const ulX = document.querySelector("ul.lines_x");
 
 const randomColors = ["blue","yellow","green","grey","white","red","pink"]
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < 100; i++){
   const random = (max,min) => Math.random() * (max - min) + min;
   const liY = document.createElement("li");
   const liX = document.createElement("li");
 
-  const delay = random(2,0.1)
-  const duration = random(24,12)
+  const delay = random(3,0.1)
+  const duration = random(10,5)
+  const height = random(90,20)
+  const width = random(90,20)
 
+  liY.style.height = `${height}vh`
   liY.style.backgroundColor = randomColors[Math.floor(Math.random() * 8)]
   liY.style.animationDelay = `${delay}s`
   liY.style.animationDuration = `${duration}s`
@@ -17,6 +20,7 @@ for(let i = 0; i < 50; i++){
     cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})
   `
 
+  liX.style.width = `${width}vw`
   liX.style.backgroundColor = randomColors[Math.floor(Math.random() * 8)]
   liX.style.animationDelay = `${delay}s`
   liX.style.animationDuration = `${duration}s`
